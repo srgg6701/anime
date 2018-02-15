51d5100310011fdbd275cc79863d1516ea34864e
Klevtsov <sergei.klevtsov@accenture.com>
2018-02-15 18:09:57 +0200

    Changing WM script

--- dev/script.js
+++ dev/script.js
@@ -1,9 +1,11 @@
 $(() => {
+    $('tr').hide();
     var dataMark = 'data-md-name-id',
         setVal = (key) => prodFieldsArray[key].forEach(txts => {
             var $td = $('td[' + dataMark + '="' + txts[1] + '"]'),
                 dataVal = $td.attr(dataMark);
             $td.prepend(`<span class="data-mark">${dataVal}</span>`);
+            $td.parent().show();
         });
     // data-md-name-id
     setVal('top');
